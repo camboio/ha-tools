a9478a342c8fa80f7ec88f4ea94fd1b921593157
cambo <anthony@cambo.io>
2017-02-10 06:04:05 +0000

    fixed some shit

--- node.js
+++ node.js
@@ -21,12 +21,47 @@ var dig = function(args, callback){
    });
 }
 
+var digA = function(args, callback){
+   dig('a +short ' + args, function(stdout) {
+      var a = stdout.split(/\n/);
+      callback(a[0]);
+   });
+}
+
+var digMX = function(args, callback){
+   dig('mx +short ' + args, function(stdout) {
+      var raw = stdout.split(/\.\n/);
+      raw.pop(); // last entry in this array is always empty
+      var mx = { 'record': [], 'priority': [], 'ip': [] };
+      async.times(raw.length, function(n, next){
+         var m = raw[n].split(/\s/);
+         mx['priority'][n] = m[0];
+         mx['record'][n] = m[1];
+         digA(m[1], function(mxip) {
+            mx['ip'][n] = mxip;
+            next();
+         });
+      }, function(err, etc) {
+         callback(mx);
+      });
+   });
+}
+
 var rdns = function(args, callback){
    var query = "'" + args + "'"; //sanitise the input dog
    exec('host ' + query + ' | grep -v "not found"', function(error, stdout, stderr){
       if(stdout.length < 1) { callback(""); return; } // no rdns
-      var rdns = stdout.match(/([\w\.]+)\.\n$/); // just get the useful shit
+      var rdns = stdout.match(/([\w\.-]+)\.\n$/); // just get the useful shit
       callback(rdns[1]);
+   });
+}
+
+
+var digC = function(args, callback){
+   dig('+short ' + args, function(stdout){
+      var cname = stdout.split(/\.?\n/);
+      cname.pop(); // last entry in this array is always empty
+      callback(cname);
    });
 }
 
@@ -56,10 +91,9 @@ app.get('/had/*', function(req, res) {
    var tasks= [];
 
    tasks.push(function(callback){ // get root ip and rdns details
-      dig('a +short ' + domain, function(stdout) {
-         var ip = stdout.split(/\n/);
-         rdns(ip[0], function(stdout) {
-            dict['root'] = { 'ip': ip[0], 'rdns': stdout };
+      digA(domain, function(ip) {
+         rdns(ip, function(stdout) {
+            dict['root'] = { 'ip': ip, 'rdns': stdout };
             callback();
          });
       });
@@ -75,33 +109,35 @@ app.get('/had/*', function(req, res) {
    });
 
    tasks.push(function(callback){ // get domain mx records
-      dig('mx +short ' + domain, function(stdout){
-         var raw = stdout.split(/\.\n/);
-         raw.pop(); // last entry in this array is always empty
-         var mx = { 'record': [], 'priority': [], 'ip': [] };
-         for(var i = 0; i < raw.length; i++) {
-            var m = raw[i].split(/\s/);
-            mx['priority'][i] = m[0];
-            mx['record'][i] = m[1];
-         }
+      digMX(domain, function(mx){
          dict['mx'] = mx;
          callback();
       });
    });
 
-   // add task to iterate through mx records and get ips for them
-
    tasks.push(function(callback){ // get www cname and ip details
       var dub = 'www.' + domain;
-      dig('+short ' + dub, function(stdout){
-         var cname = stdout.split(/\.?\n/);
-         dict['www'] = { 'cname' : "", 'ip': "" };
-         cname.pop(); // last entry in this array is always empty
-         if(cname.length > 1) { //if cname and ip
-            dict['www']['cname'] = cname[0];
-            dict['www']['ip'] = cname[1];
+      digC(dub, function(stdout){
+         dict['www'] = { 'cname': "", 'ip': "" };
+         if(stdout.length > 1) { //if cname and ip
+            dict['www']['cname'] = stdout[0];
+            dict['www']['ip'] = stdout[1];
          }else{
-            dict['www']['ip'] = cname[0];
+            dict['www']['ip'] = stdout[0];
+         }
+         callback();
+      });
+   });
+
+   tasks.push(function(callback){ // get mail. a record and cname details
+      var mail = 'mail.' + domain;
+      digC(mail, function(stdout) {
+         dict['mail'] = { 'cname': "", 'ip': "" };
+         if(stdout.length > 1) { // if cname and ip
+            dict['mail']['cname'] = stdout[0];
+            dict['mail']['ip'] = stdout[1];
+         }else{
+            dict['mail']['ip'] = stdout[0];
          }
          callback();
       });
